7364212b5c42df01224af033b3c4b46a04e79108
trangdo <trangdo191024@gmail.com>
2023-01-25 22:27:19 +0700

    sort array

--- tongabc.py
+++ tongabc.py
@@ -1,4 +1,23 @@
-a = int(input("nhap so nguyen a: "))
-b = int(input("nhap so nguyen b: "))
-c = int(input("nhap so nguyen c: "))
-print(a + b + c)
+"""a = input("nhap so nguyen a: ")
+b = input("nhap so nguyen b: ")
+c = input("nhap so nguyen c: ")
+print(float(a + b + c))
+
+x = input()
+y = input()
+print(x + y)"""
+
+length = int(input())
+numbers = []
+
+for i in range(0, length):
+    numbers.append(int(input()))
+
+for i in range(0, length):
+    for j in range(i+1, length):
+        if (numbers[i] > numbers[j]):
+            tmp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = tmp
+
+print(numbers)
