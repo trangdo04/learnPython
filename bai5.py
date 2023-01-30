56035664164048c48edd095909690d70822ac81a
trangdo <trangdo191024@gmail.com>
2023-01-30 22:22:26 +0700

    loops

---
+++ bai5.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i * i)
+
+# https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
