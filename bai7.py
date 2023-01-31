7ff69ea33d3c52a9a92706bcc89f6015f8255d7e
trangdo <trangdo191024@gmail.com>
2023-01-31 21:50:17 +0700

    Print function

---
+++ bai7.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    s = ""
+    for i in range(n):
+        s = s + str(i+1)
+    
+    print(s)
+
+# https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
