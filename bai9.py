4473d29a880757de6c34d540d6b71acaec1d4694
trangdo <trangdo191024@gmail.com>
2023-02-01 22:31:13 +0700

    sWAP cASE

---
+++ bai9.py
@@ -0,0 +1,10 @@
+def swap_case(s):
+    x = s.swapcase()
+    return x
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+# https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
